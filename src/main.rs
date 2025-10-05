cd11d2aa27b8f8484ad8220fcdeb7b2d471890a8
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2025-10-05 13:48:21 +1100

    feat: allow passing c file on disk rather than repl only

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::io::stdin;
+use std::{
+    env::args,
+    fs::{read, read_to_string},
+    io::stdin,
+};
 
 use codespan_reporting::{
     diagnostic::{Diagnostic, Label},
     files::SimpleFiles,
-    term::{
-        self,
-        termcolor::{ColorChoice, StandardStream},
-    },
+    term,
 };
 
 use crate::{lexer::Lexer, parser::Parser};
@@ -16,8 +17,15 @@ mod parser;
 mod span;
 
 fn main() {
-    let writer = StandardStream::stderr(ColorChoice::Always);
-    let config = codespan_reporting::term::Config::default();
+    let codespan_writer =
+        term::termcolor::StandardStream::stderr(term::termcolor::ColorChoice::Always);
+
+    let codespan_config = codespan_reporting::term::Config::default();
+
+    if let Some(path) = args().nth(1) {
+        let buf = read_to_string(path).expect("Must pass a valid path to read from");
+        return parse_program(&buf, &codespan_writer, &codespan_config);
+    }
 
     let stdin = stdin();
 
@@ -30,25 +38,39 @@ fn main() {
             if buf.trim_end().ends_with(";;") {
                 break;
             }
+
+            parse_program(&buf, &codespan_writer, &codespan_config);
         }
-
-        let lexer = Lexer::new(&buf);
-        let parser = Parser::new(lexer);
-
-        let program = match parser.parse() {
-            Ok(program) => program,
-            Err(err) => {
-                let mut files = SimpleFiles::new();
-                let file = files.add("main.c", &buf);
-
-                let diagnostic = Diagnostic::error()
-                    .with_label(Label::primary(file, err.location).with_message(err.kind));
-
-                term::emit(&mut writer.lock(), &config, &files, &diagnostic).unwrap();
-                continue;
-            }
-        };
-
-        println!("Program: {program}");
     }
+}
+
+fn parse_program(
+    buf: &str,
+    codespan_writer: &term::termcolor::StandardStream,
+    codespan_config: &term::Config,
+) {
+    let lexer = Lexer::new(buf);
+    let parser = Parser::new(lexer);
+
+    let program = match parser.parse() {
+        Ok(program) => program,
+        Err(err) => {
+            let mut files = SimpleFiles::new();
+            let file = files.add("main.c", &buf);
+
+            let diagnostic = Diagnostic::error()
+                .with_label(Label::primary(file, err.location).with_message(err.kind));
+
+            term::emit(
+                &mut codespan_writer.lock(),
+                codespan_config,
+                &files,
+                &diagnostic,
+            )
+            .unwrap();
+            return;
+        }
+    };
+
+    println!("Program: {program}");
 }
