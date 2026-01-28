fb05e47770f191726d05e6ba2310eb388ad19f50
orca <35243139+thennothinghappened@users.noreply.github.com>
2026-01-28 23:04:47 +1100

    fix(c): annotate panic & exit as [[noreturn]] in header

--- include/armlite.h
+++ include/armlite.h
@@ -8,6 +8,11 @@ void WriteString(char* string);
 void WriteSignedNum(int num);
 void WriteUnsignedNum(unsigned int num);
 void ReadString(char* outputString);
+
+[[noreturn]]
 void Panic(char* message);
+
+[[noreturn]]
 void Exit(int code);
+
 void memcpy(void *destination, void *source, size_t count);
